452da8b5f76e9c74215919e212f7a4367254042c
Resul Lomen <130861235+rglomen@users.noreply.github.com>
2024-10-16 20:38:24 +0300

    Proje dosyası ekle.

---
+++ lchatserver/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace lchatserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
+            this.Klist = new System.Windows.Forms.ListBox();
+            this.konsole = new System.Windows.Forms.ListBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.porttxt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            this.metroTabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Controls.Add(this.metroTabPage3);
+            this.metroTabControl1.Location = new System.Drawing.Point(23, 63);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(767, 375);
+            this.metroTabControl1.TabIndex = 0;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.Controls.Add(this.porttxt);
+            this.metroTabPage1.Controls.Add(this.metroButton2);
+            this.metroTabPage1.Controls.Add(this.metroButton1);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(759, 336);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Server Kontrol";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.Klist);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(759, 336);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Kulanıcı Listesi";
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            // 
+            // metroTabPage3
+            // 
+            this.metroTabPage3.Controls.Add(this.konsole);
+            this.metroTabPage3.HorizontalScrollbarBarColor = true;
+            this.metroTabPage3.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage3.Name = "metroTabPage3";
+            this.metroTabPage3.Size = new System.Drawing.Size(759, 336);
+            this.metroTabPage3.TabIndex = 2;
+            this.metroTabPage3.Text = "Console";
+            this.metroTabPage3.VerticalScrollbarBarColor = true;
+            // 
+            // Klist
+            // 
+            this.Klist.FormattingEnabled = true;
+            this.Klist.Location = new System.Drawing.Point(3, 3);
+            this.Klist.Name = "Klist";
+            this.Klist.Size = new System.Drawing.Size(753, 329);
+            this.Klist.TabIndex = 2;
+            this.Klist.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // konsole
+            // 
+            this.konsole.FormattingEnabled = true;
+            this.konsole.Location = new System.Drawing.Point(3, 3);
+            this.konsole.Name = "konsole";
+            this.konsole.Size = new System.Drawing.Size(747, 329);
+            this.konsole.TabIndex = 2;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(3, 12);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(131, 23);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Server Başlat";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(3, 41);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(131, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Server Durdur";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // porttxt
+            // 
+            this.porttxt.Location = new System.Drawing.Point(365, 12);
+            this.porttxt.Name = "porttxt";
+            this.porttxt.Size = new System.Drawing.Size(202, 23);
+            this.porttxt.TabIndex = 4;
+            this.porttxt.Text = "5000";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(316, 16);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(43, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "PORT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "Form1";
+            this.Text = "L-CHAT SERVER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage2.ResumeLayout(false);
+            this.metroTabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private System.Windows.Forms.ListBox Klist;
+        private MetroFramework.Controls.MetroTabPage metroTabPage3;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.ListBox konsole;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox porttxt;
+    }
+}
+
---
+++ lchatserver/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace lchatserver
+{
+    public partial class Form1 : MetroForm
+    {
+        private UdpClient udpServer;
+        private IPEndPoint remoteEndPoint;
+        private bool isRunning;
+        private int port;
+        private readonly object lockObj = new object();
+        private List<IPEndPoint> connectedClients = new List<IPEndPoint>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            konsole.Items.Add("Program Yüklendi");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void StartServer()
+        {
+            int port; // Port numarasını tutacak değişken
+                  
+            if (int.TryParse(porttxt.Text, out port))
+            {
+                udpServer = new UdpClient(new IPEndPoint(IPAddress.Any, port)); // TextBox'tan alınan port ile dinle
+                udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                remoteEndPoint = new IPEndPoint(IPAddress.Any, port); // Hedef IP
+                isRunning = true;
+
+                // Form durumunu güncelle
+                konsole.Items.Add("Sunucu başlatıldı, dinleniyor...");
+                
+
+                Task.Run(() => ListenForClients()); // Arka planda dinleme başlat
+            }
+            else
+            {
+                MessageBox.Show("Geçerli bir port numarası girin."); // Hata mesajı
+            }
+        }
+        private void ListenForClients()
+        {
+            while (true)
+            {
+                byte[] receivedData = udpServer.Receive(ref remoteEndPoint); // Gelen veriyi al
+                string clientInfo = $"{remoteEndPoint.Address}:{remoteEndPoint.Port}";
+
+                // İstemci listesini güncelle
+                if (!Klist.Items.Contains(clientInfo))
+                {
+                    Klist.Invoke((MethodInvoker)delegate {
+                        Klist.Items.Add(clientInfo);
+                    });
+                }
+
+                // Gelişmiş işlem (Ses gönderme) ekleyebilirsin
+                konsole.Items.Add($"Veri alındı: {receivedData.Length} bytes - {clientInfo}");
+            }
+            while (isRunning)
+            {
+                // İstemci bağlantısını dinle
+                var receivedData = udpServer.Receive(ref remoteEndPoint);
+
+                // Yeni istemci ekleme
+                if (!connectedClients.Contains(remoteEndPoint))
+                {
+                    connectedClients.Add(remoteEndPoint);
+                    konsole.Items.Add(remoteEndPoint.ToString()); // İstemci listesini güncelle
+                }
+
+                // Alınan veriyi diğer istemcilere gönderme
+                BroadcastData(receivedData);
+            }
+            while (isRunning)
+            {
+                try
+                {
+                    var receivedData = udpServer.Receive(ref remoteEndPoint);
+                    // Diğer işlemler
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show($"Socket hatası: {ex.Message}");
+                    break; // Hata alındığında döngüden çık
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Hata: {ex.Message}");
+                    break; // Diğer hatalar için
+                }
+            }
+        }
+        private void BroadcastData(byte[] data)
+        {
+            foreach (var client in connectedClients)
+            {
+                udpServer.Send(data, data.Length, client);
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+        private void StopServer()
+        {
+            lock (lockObj) // Thread güvenliği için kilit kullanıyoruz
+            {
+                if (isRunning)
+                {
+                    try
+                    {
+                        if (udpServer != null)
+                        {
+                            udpServer.Close();
+                            udpServer = null; // null yaparak tekrar kapatmayı önle
+                        }
+
+                        isRunning = false;
+                        konsole.Items.Add("Sunucu durduruldu.");
+                    }
+                    catch (SocketException ex)
+                    {
+                        MessageBox.Show($"Socket hatası: {ex.Message}"); // Hata mesajını göster
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Hata: {ex.Message}"); // Diğer hataları göster
+                    }
+                }
+            }
+        }
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            StopServer();
+        }
+
+    }
+}
